e9e62a322cef668f1975009f0da1775bb32bbba9
t-aerackk <aenjaal@mail.com>
2023-06-23 20:56:27 +0545

    Ex 1

---
+++ Var.py
@@ -0,0 +1,32 @@
+#Variables on python
+#Variables are named containers on computer storage for storing values that can be manipulated
+
+#1. create two variables assign values and swap values
+# A=10
+# B=20
+# print(A,B) ---> Output 10,20
+# a=B
+# b=A
+# print(a,b) ---> Output 20, 10
+
+#Another way
+# A=10
+# B=20
+# A,B=B,A
+# print("A=",A)
+# print("B=",B)
+
+#2. Usng function to concatenate strings
+# name="anjal"
+# age=25
+# address="godawari"
+# def details():
+#     print (f"my name is {name} living in {address} with {age} years.")
+# details()
+
+#3. Type conversion from one type to another
+# name="anjal"
+# age="25"
+# print(int(age)+2)
+
+#4. 
